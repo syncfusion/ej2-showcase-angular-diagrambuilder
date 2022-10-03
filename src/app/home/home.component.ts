9284c4b4d163a9f29982aa9c422c099309e7f964
SF3741 <vivisa.ganesan@syncfusion.com>
2022-10-03 18:15:58 +0530

    Angular diagram builder

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -720,9 +720,9 @@ export class HomeComponent implements AfterViewInit {
         } else if (args.item.text === 'Bezier') {
             diagram.drawingObject = { type: 'Bezier', style: { strokeWidth: 2 } };
         }
-        // else if(args.item.text === 'FreeHand'){
-        //     diagram.drawingObject = { type: 'Freehand', style: { strokeWidth: 2 } };
-        // }
+        else if(args.item.text === 'FreeHand'){
+            diagram.drawingObject = { type: 'Freehand', style: { strokeWidth: 2 } };
+        }
         diagram.tool = DiagramTools.ContinuousDraw;
         diagram.clearSelection();
         this.removeSelectedToolbarItem();
